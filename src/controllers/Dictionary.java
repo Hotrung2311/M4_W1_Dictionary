d1f36a0241060d314c7686171c166aa00f9d4e14
Hotrung2311 <trungho2311@gmail.com>
2020-06-16 16:26:06 +0700

    dictionary

---
+++ src/controllers/Dictionary.java
@@ -0,0 +1,35 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
+
+@Controller
+public class Dictionary {
+
+    public HashMap<String, String> myDictionary = new HashMap<>();
+
+    public String search(String word){
+        String result = "not found";
+        result = myDictionary.get(word);
+        return result;
+    }
+
+    @PostMapping("/dictionary")
+    public String searchWord(@RequestParam String word, Model model){
+        myDictionary.put("Hello", "Xin chào");
+        myDictionary.put("Fuck", "Đụ");
+        model.addAttribute("word", search(word));
+        return "result";
+    }
+
+    @GetMapping("/dictionary")
+    public String showMeaning(@RequestParam String word,Model model){
+        model.addAttribute("word", search(word));
+        return "result";
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ADMIN
+  Date: 6/16/2020
+  Time: 3:35 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>Dictionary</title>
+  </head>
+  <body>
+  <form method="post" action="/dictionary">
+    <input name="word" type="text">
+    <input type="submit" value="search">
+  </form>
+  </body>
+</html>
